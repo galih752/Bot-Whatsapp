c576eb030fc271c6338ac5ae624095a999dc1645
galih752 <rd.galih752@gmail.com>
2024-02-28 13:31:59 +0700

    Update

--- index.js
+++ index.js
@@ -20,13 +20,27 @@ client.on('message', async (message) => {
 });
 
 
+// Informasi Kumpul atau Pertemuan 
 client.on('message', async (message) => {
     const [action, tanggal, waktu, tempat, pakaian, note] = message.body.split(';');
 	if (action.trim() =='!info') {
+
+        const currentHour = new Date().getHours();
+        
+        // Menentukan salam berdasarkan waktu
+        let greeting = '';
+        if (currentHour < 12) {
+            greeting = 'Selamat pagi';
+        } else if (currentHour < 18) {
+            greeting = 'Selamat siang';
+        } else {
+            greeting = 'Selamat sore';
+        }
+
 		await message.reply(`🔊🔊
-Assalamualaikum warahmatullahi wa barakatuh,
-Selamat pagi, Salam Pramuka! 
-Diberitahukan kepada seluruh anggota untuk hadir pada:
+Assalamualaikum warahmatullahi wabarakatuh,
+${greeting}, Salam Pramuka! 
+Diberitahukan kepada seluruh anggota Dewan Kerja Ranting Padalarang diwajibkan hadir pada:
 
 📆 Hari/tanggal : ${tanggal}
 ⏰ Waktu : ${waktu}
@@ -35,10 +49,69 @@ Diberitahukan kepada seluruh anggota untuk hadir pada:
 
 📝 Catatan : ${note}
 
-Demikian pengumuman ini saya sampaikan saya harap seluruh anggota dapat hadir pada kegiatan tersebut di atas, Terimakasih.
+Demikian pengumuman ini saya sampaikan saya harap seluruh anggota dapat hadir pada kegiatan tersebut di atas, sesuai dengan waktu yang telah di tentukan, Terimakasih.
 
 Created by ${await (await message.getContact()).getFormattedNumber()}`);
 	}
+});
+
+//Pengingat Kumpul
+client.on('message', async (message) => {
+    const [action, tanggal, waktu, tempat, pakaian, note] = message.body.split(';');
+	if (action.trim() =='!warning') {
+
+        const currentHour = new Date().getHours();
+        
+        // Menentukan salam berdasarkan waktu
+        let greeting = '';
+        if (currentHour < 12) {
+            greeting = 'Selamat pagi';
+        } else if (currentHour < 18) {
+            greeting = 'Selamat siang';
+        } else {
+            greeting = 'Selamat sore';
+        }
+
+		await message.reply(`🔊🔊
+Assalamualaikum warahmatullahi wabarakatuh,
+${greeting}, Salam Pramuka! 
+Mengingatkan kembali kepada seluruh anggota Dewan Kerja Ranting Padalarang diwajibkan hadir pada:
+
+📆 Hari/tanggal : ${tanggal}
+⏰ Waktu : ${waktu}
+📍 Tempat : ${tempat}
+👔 Pakaian : ${pakaian}
+
+📝 Catatan : ${note}
+
+Demikian pengumuman ini saya sampaikan saya harap seluruh anggota dapat hadir pada kegiatan tersebut di atas, sesuai dengan waktu yang telah di tentukan, Terimakasih.
+
+Created by ${await (await message.getContact()).getFormattedNumber()}`);
+	}
+});
+
+//List
+client.on('message', async (message) => {
+    const [action, kegiatan] = message.body.split(';');
+    if (action.trim() =='!list') {
+        await message.reply(`List kehadiran kegiatan ${kegiatan} :
+1.
+2.
+3.
+4.
+5.
+dst.
+
+List tidak hadir - alasan :
+1.
+2.
+3.
+4.
+5.
+dst.
+
+created by ${await (await message.getContact()).getFormattedNumber()}`);
+    }
 });
  
 
